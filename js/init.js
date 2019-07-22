d68c8e13d855267cefa91496705225dfa93ce822
charlessamoy2 <53179157+charlessamoy2@users.noreply.github.com>
2019-07-22 20:25:51 +0800

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+  	$('.materialboxed').materialbox();
+    $('.collapsible').collapsible();
+	$('.tabs').tabs();
+	$('.slider').slider();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
